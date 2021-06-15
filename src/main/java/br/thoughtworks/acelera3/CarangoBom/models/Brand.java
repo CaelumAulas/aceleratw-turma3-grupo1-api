96c4de78d8cf0088d112fa06faff3e9861ce3a80
Priscila Ribeiro <trabalho.pri@gmail.com>
2021-06-15 11:39:56 -0300

    Anotações e validação dos campos de Brand e Vehicle

--- src/main/java/br/thoughtworks/acelera3/CarangoBom/models/Brand.java
+++ src/main/java/br/thoughtworks/acelera3/CarangoBom/models/Brand.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Brand {
@@ -12,6 +14,8 @@ public class Brand {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotNull
+	@NotEmpty
 	private String name;
 	
 	public Brand() { }
